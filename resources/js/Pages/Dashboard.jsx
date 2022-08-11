6ec1eb69fcf41e4e2164e68f5a25fc2b41bbd584
disman <disman810@gmail.com>
2022-08-12 00:56:36 +0800

    Tambah modul create berita

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
+import { Inertia } from '@inertiajs/inertia';
 
 export default function Dashboard(props) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [isNotif, setIsNotif] = useState(false)
+
+    const handleSubmit = () => {
+        const data = {
+            title, description, category
+        }
+        Inertia.post('/news', data)
+        setIsNotif(true)
+        setTitle()
+        setDescription('')
+        setCategory('')
+    }
+
+    console.log('props last: ', props)
+
     return (
         <Authenticated
             auth={props.auth}
@@ -13,8 +32,34 @@ export default function Dashboard(props) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">You're logged in!</div>
+                    <div className="p-6 bg-white border-b border-gray-200">
+                        {isNotif && <div class="alert alert-success shadow-lg">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                <span>{props.flash.message}</span>
+                            </div>
+                        </div>
+                        }
+
+                        <input type="text" placeholder="Title" className="m-2 input input-bordered w-full" onChange={(title) => setTitle(title.target.value)} value={title} />
+                        <input type="text" placeholder="Description" className="m-2 input input-bordered w-full" onChange={(description) => setDescription(description.target.value)} value={description} />
+                        <input type="text" placeholder="Category" className="m-2 input input-bordered w-full" onChange={(category) => setCategory(category.target.value)} value={category} />
+                        <button className='btn btn-outline m-2' onClick={() => handleSubmit()}>Post</button>
+                    </div>
+                </div>
+                <div className="p-4">
+                    <div className="card w-full lg:w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title">
+                                Title
+                                <div className="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>Deskripsi</p>
+                            <div className="card-actions justify-end">
+                                <div className="badge badge-inline">Category</div>
+                                <div className="badge badge-outline">Author</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,18 @@ use Inertia\Inertia;
 //     ]);
 // });
 
-Route::get('/', [NewsController::class, 'index']);
 
-Route::get('/welcome', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/welcome', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::get('/', [NewsController::class, 'index']);
+Route::post('/news', [NewsController::class, 'store']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
